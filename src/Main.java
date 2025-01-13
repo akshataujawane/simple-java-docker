773c489c5fbc57dc638d6c1776fd24179708910a
Akshata <103401481+akshataujawane@users.noreply.github.com>
2025-01-13 14:14:07 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
-        System.out.println("Hi my name is akshata ! Current date: 8 Jan 2025 " + currentDate);
+        System.out.println("Hi my name is akshata  i am looking for the job! Current date: 8 Jan 2025 " + currentDate);
     }
 }
